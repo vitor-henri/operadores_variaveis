0bfd6688173302b058846d500de51a480fdd5b78
Vitor <vitorfonsecaxs@gmail.com>
2025-01-28 16:28:26 -0300

    add: acrescimo e desconto feitos

--- v3/operadores-v3.js
+++ v3/operadores-v3.js
@@ -19,7 +19,7 @@ function somar(){
             <li>Operação: <span>SOMA</span> </li>
             <li>Valor 01 digitado pelo usuário: <span> ${valor1} </span> </li>
             <li>Valor 02 digitado pelo usuário: <span>${valor2}</span> </li>       
-            <li>O resultado da operação é ${valor1} + ${valor2} = <span>${valor1 + valor2}</span> </li>
+            <li>O resultado da operação é ${valor1} + ${valor2} = <span>${(valor1 + valor2).toFixed(2)}</span> </li>
         </ul>
         `
     }
@@ -43,11 +43,11 @@ function subtrair(){
         resultado.innerHTML = 
         `
         <ul>
-            <li>Operação: <span>SOMA</span> </li>
-            <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-            <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
-            <li>O resultado da operação é ${v1} + ${v2} = <span>${v1 - v2}</span> </li>
-            </ul>
+            <li>Operação: <span>SUBTRAÇÃO</span> </li>
+            <li>Valor 01 digitado pelo usuário: <span> ${valor1} </span> </li>
+            <li>Valor 02 digitado pelo usuário: <span>${valor2}</span> </li>       
+            <li>O resultado da operação é ${valor1} - ${valor2} = <span>${(valor1 - valor2).toFixed(2)}</span> </li>
+        </ul>
         `
     }
 }
@@ -70,11 +70,11 @@ function multiplicar(){
         resultado.innerHTML = 
         `
         <ul>
-            <li>Operação: <span>SOMA</span> </li>
-            <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-            <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
-            <li>O resultado da operação é ${v1} + ${v2} = <span>${v1 * v2}</span> </li>
-            </ul>
+            <li>Operação: <span>MULTIPLICAÇÃO</span> </li>
+            <li>Valor 01 digitado pelo usuário: <span> ${valor1} </span> </li>
+            <li>Valor 02 digitado pelo usuário: <span>${valor2}</span> </li>       
+            <li>O resultado da operação é ${valor1} * ${valor2} = <span>${(valor1 * valor2).toFixed(2)}</span> </li>
+        </ul>
         `
     }
 };
@@ -97,17 +97,17 @@ function dividir(){
         resultado.innerHTML = 
         `
         <ul>
-            <li>Operação: <span>SOMA</span> </li>
-            <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-            <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
-            <li>O resultado da operação é ${v1} + ${v2} = <span>${v1 * v2}</span> </li>
-            </ul>
+            <li>Operação: <span>DIVISÃO</span> </li>
+            <li>Valor 01 digitado pelo usuário: <span> ${valor1} </span> </li>
+            <li>Valor 02 digitado pelo usuário: <span>${valor2}</span> </li>       
+            <li>O resultado da operação é ${valor1} / ${valor2} = <span>${(valor1 / valor2).toFixed(2)}</span> </li>
+        </ul>
         `
     }
 };
 
 function porcentagem(){
-     //  são 2 variaveis que guardam o valor do campo que o usuario digitou
+    //  são 2 variaveis que guardam o valor do campo que o usuario digitou
     const valor1 = parseFloat(document.getElementById('valor01').value);
     const valor2 = parseFloat(document.getElementById('valor02').value);
     // transformar os container em flex para aparecer o resultado como um container
@@ -116,7 +116,7 @@ function porcentagem(){
     // verificando se o usuario digitou algum campo em branco e dando um aviso se for True
     if (valor1 === '' || valor2 === ''){
         // alerta para o usuario
-        alert('Valor 1 ou Valor 2 esta em branco, digite novamente')
+        alert('Valor 1 ou Valor 2 esta em branco, digite novamente');
     }
     // caso o usuario digite 2 digitos ele faz o calculo direto e manda para o conteiner
     else{
@@ -124,14 +124,74 @@ function porcentagem(){
         resultado.innerHTML = 
         `
         <ul>
-            <li>Operação: <span>SOMA</span> </li>
-            <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-            <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
-            <li>O resultado da operação é ${v1} + ${v2} = <span>${v1*v2/100}</span> </li>
-            </ul>
+            <li>Operação: <span>PORCENTAGEM</span> </li>
+            <li>Valor 01 digitado pelo usuário: <span> ${valor1} </span> </li>
+            <li>Valor 02 digitado pelo usuário: <span>${valor2}</span> </li>       
+            <li>O resultado da operação é ${valor1} % ${valor2} = <span>${(valor1*valor2/100).toFixed(2)}</span> </li>
+        </ul>
         `
     }
 };
+
+function desconto(){
+    //  são 2 variaveis que guardam o valor do campo que o usuario digitou
+    const preco = parseFloat(document.getElementById('valor01').value);
+    const desconto = parseFloat(document.getElementById('valor02').value);
+    // transformar os container em flex para aparecer o resultado como um container
+    document.getElementById('resultado').style.display = 'flex';
+    const resultado = document.getElementById('resultado');
+    // conta para calcular o desconto
+    let valor_desconto = preco * (desconto/100);
+    let valor_pagar = preco - valor_desconto;
+    // verificando se o usuario digitou algum campo em branco e dando um aviso se for True
+    if (preco === '' || desconto === ''){
+        // alerta para o usuario
+        alert('Valor 1 ou Valor 2 esta em branco, digite novamente');
+    }
+    // caso o usuario digite 2 digitos ele faz o calculo direto e manda para o conteiner
+    else{
+        // usando o inner para rescrever o HTML trocando as variaveis pelos numeros
+        resultado.innerHTML = 
+        `
+        <ul>
+            <li>Operação: <span>DESCONTO</span> </li>
+            <li>Valor 01 valor da compra: <span> ${preco} </span> </li>
+            <li>Valor 02 valor de desconto: <span>${valor_desconto}</span> </li>       
+            <li>O resultado da operação é ${preco} - ${valor_desconto} = <span>${(valor_pagar).toFixed(2)}</span> </li>
+        </ul>
+        `
+    }
+}
+
+function acrescimo(){
+    //  são 2 variaveis que guardam o valor do campo que o usuario digitou
+    const preco = parseFloat(document.getElementById('valor01').value);
+    const acrescimo = parseFloat(document.getElementById('valor02').value);
+    // transformar os container em flex para aparecer o resultado como um container
+    document.getElementById('resultado').style.display = 'flex';
+    const resultado = document.getElementById('resultado');
+    // conta para calcular o acrescimo
+    let valor_acrescimo = preco * (acrescimo/100);
+    let valor_pagar = preco + valor_acrescimo;
+    // verificando se o usuario digitou algum campo em branco e dando um aviso se for True
+    if (preco === '' || desconto === ''){
+        // alerta para o usuario
+        alert('Valor 1 ou Valor 2 esta em branco, digite novamente');
+    }
+    // caso o usuario digite 2 digitos ele faz o calculo direto e manda para o conteiner
+    else{
+        // usando o inner para rescrever o HTML trocando as variaveis pelos numeros
+        resultado.innerHTML = 
+        `
+        <ul>
+            <li>Operação: <span>ACRESCIMO</span> </li>
+            <li>Valor 01 valor da compra: <span> ${preco} </span> </li>
+            <li>Valor 02 valor de desconto: <span>${valor_acrescimo}</span> </li>       
+            <li>O resultado da operação é ${preco} + ${valor_acrescimo} = <span>${(valor_pagar).toFixed(2)}</span> </li>
+        </ul>
+        `
+    }
+}
 
 function limpar(){
     //  o conteiner que esta com o id resultado ele vai deixar de ser flex e vai ser none, ou seja vai sumir
